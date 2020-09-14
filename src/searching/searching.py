0a1201c028fe97156066197b1bcf317fdd80442f
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2020-09-14 11:36:38 -0700

    both search tests are passing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,27 @@
 def linear_search(arr, target):
     # Your code here
-
-
-    return -1   # not found
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return arr.index(arr[i])
+    
+    return -1
+    
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
     # Your code here
+    left = 0
+    right = len(arr) - 1
 
-
+    while left <= right:
+        midpoint = (right + left) // 2
+        if arr[midpoint] == target:
+            return arr.index(target)
+        elif arr[midpoint] > target:
+            right = midpoint - 1
+        elif arr[midpoint] < target:
+            left = midpoint + 1
+    
     return -1  # not found
